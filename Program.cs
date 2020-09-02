12bb085a4c4777ff4a479fb746df3a4b4b09964f
Vince Oblak <voblak2@gmail.com>
2020-09-01 22:23:16 -0500

    initial commit for lesson 1 hands on c#

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace lesson1HandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isSunny = true;
+
+            var wear = "Wear sunglasses.";
+            var dontWear = "You don't need to wear sunglasses.";
+
+                if (isSunny)
+                {
+                    Console.WriteLine(wear);
+                }   else 
+                    {
+                        Console.WriteLine(dontWear);
+                    }
+
+            bool atBeach = true;
+
+            var sunblock = "Sunblock is a good idea.";
+            var noSunblock = "Sunblock isn't needed.";
+
+                if (atBeach)
+                {
+                    Console.WriteLine(sunblock);
+                }   else 
+                    {
+                        Console.WriteLine(noSunblock);
+                    }
+   
+        }
+    }
+}
